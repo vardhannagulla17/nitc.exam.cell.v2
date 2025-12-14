08f15e2a6ba64da91f9be5877bacad9c31965f26
vardhannagulla17 <vardhan.sai1117@gmail.com>
2025-12-14 15:10:12 +0530

    Add missing make_response import

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, flash, session, send_file, current_app
+from flask import Flask, render_template, request, redirect, url_for, flash, session, send_file, current_app, make_response
 from werkzeug.utils import secure_filename
 import os
 import sys
